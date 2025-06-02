e31eaa7cb8f29e154b7ee610491507541b5d34e0
Marcela Dias <ddiasmarcela@gmail.com>
2025-06-02 16:32:45 -0300

    Página de formações

---
+++ meu-portfolio/src/app/componentes/cardFormacao/index.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function CardFormacao ({ conteudo, background }) {
+    return (
+        <div className={`flex items-center justify-start gap-12 p-6 font-bold bg-[${background}] rounded-full max-w-[800px] w-11/12`}>
+            <Image src="formacao.svg" height={76} width={76} alt="Jogos ícone" className=""/> 
+            {conteudo}
+        </div>
+    )
+}
--- meu-portfolio/src/app/componentes/header/index.js
+++ meu-portfolio/src/app/componentes/header/index.js
@@ -8,7 +8,7 @@ export default function Header () {
                     <li><Link href="/" className="w-24 bg-pink-300 text-white duration-200 ease-in-out hover:text-pink-300 hover:bg-white border-1 border-pink-300 rounded-sm p-2">Início</Link></li>
                     <li><Link href="/perfil" className="w-24 bg-pink-300 text-white duration-200 ease-in-out hover:text-pink-300 hover:bg-white border-1 border-pink-300 rounded-sm p-2">Perfil</Link></li>
                     <li><Link href="/sobre" className="w-24 bg-pink-300 text-white duration-200 ease-in-out hover:text-pink-300 hover:bg-white border-1 border-pink-300 rounded-sm p-2">Sobre</Link></li>
-                    <li><Link href="/formacao" className="w-24 bg-pink-300 text-white duration-200 ease-in-out hover:text-pink-300 hover:bg-white border-1 border-pink-300 rounded-sm p-2">Formações</Link></li>
+                    <li><Link href="/formacoes" className="w-24 bg-pink-300 text-white duration-200 ease-in-out hover:text-pink-300 hover:bg-white border-1 border-pink-300 rounded-sm p-2">Formações</Link></li>
                     <li><Link href="/jogos" className="w-24 bg-pink-300 text-white duration-200 ease-i   n-out hover:text-pink-300 hover:bg-white border-1 border-pink-300 rounded-sm p-2">Jogos</Link></li>
                     <li><Link href="/profissional" className="w-24 bg-pink-300 text-white duration-200 ease-in-out hover:text-pink-300 hover:bg-white border-1 border-pink-300 rounded-sm p-2">Profissional</Link></li>
                 </ul>
